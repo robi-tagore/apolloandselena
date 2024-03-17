c56768062ae503ab0710b280646a4810d6c73ffc
robitagore <duibigha@gmail.com>
2024-03-17 11:08:27 +0600

    m mm mmm mmmm mmmmm

--- src/packages/selena/ENVs.tsx
+++ src/packages/selena/ENVs.tsx
@@ -3,7 +3,7 @@
 // const API_PATH = 'http://localhost:13169';
 
 // const API_PATH = "https://apolloforselena.onrender.com";
-const API_PATH = process.env.API_PATH_M;
+const API_PATH = "https://apollo-m.onrender.com";
 export default {
     LOAD_FORMATS_PATH: API_PATH + '/loadformats',
     DOWNLOAD_PATH: API_PATH + '/download'
